42995eef151de84b65e318b64dc74a847cecc8cb
Aman Chaurasiya <63119820+aman-chaurasiya@users.noreply.github.com>
2023-07-03 15:59:56 +0000

    Data types done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,3 +2,20 @@
 
 // alert(3+3) //we are useing nodejs not browser
 
+let name = "Aman"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
