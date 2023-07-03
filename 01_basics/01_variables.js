d188a0ec31490d159607640b48d21e4fb604dc9a
Aman Chaurasiya <63119820+aman-chaurasiya@users.noreply.github.com>
2023-07-03 15:33:44 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553;
+let accountEmail="test@gmail.com"
+var accountPassword='123456789'
+accountCity="jaipur"
+let accountState
+
+// accountId=222 //not allowed
+
+accountEmail="jsd@gmail.com"
+accountPassword='jfjasdk'
+accountCity='bhadohi'
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,4 @@
+'use strict' // treate all code as a newer version of javascript
+
+// alert(3+3) //we are useing nodejs not browser
+
