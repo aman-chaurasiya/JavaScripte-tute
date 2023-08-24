d7a07a9dd240bcc951a1c4c45b7cb9c5df5c0858
Aman Chaurasiya <63119820+aman-chaurasiya@users.noreply.github.com>
2023-08-24 16:35:36 +0000

    sdafasdf

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+// let score=33
+let score='33'
+// let score=null
+// let score=undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+"33"=33
+"33abd"= NaN type=number
+true=1
+false=0
+" "=false
+"asdf"=true
+*/
