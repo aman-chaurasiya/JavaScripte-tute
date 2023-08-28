1be759716daf44514cb090be2f3826982f06beb9
Aman Chaurasiya <63119820+aman-chaurasiya@users.noreply.github.com>
2023-08-28 16:37:49 +0000

    Srting done

---
+++ 01_basics/05_String.js
@@ -0,0 +1,44 @@
+const name="aman-sunny"
+const repoCount=50
+
+// console.log(name.toUpperCase() + repoCount+ "value");
+
+
+ // string interpolation
+// console.log(`Hello my name ${name.toUpperCase()} and my repo rate is ${repoCount}`)
+// string declaration in javascript
+
+
+const gameName=new String('Chaurasiya-Aman-sunny-ashish')
+
+// console.log(typeof gameName);
+// console.log(typeof name);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(0));
+
+// console.log(gameName.indexOf('A'));
+
+// const newString=gameName.substring(0,4)
+// console.log(newString)
+
+
+
+// const anotherString=gameName.slice(-15,4)
+// console.log(anotherString);
+const newString1="       Ashish    Amansunny    "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+const url="https://aman.com/hitesh%20sunny"
+
+console.log(url.replace('%20','--'));
+
+console.log(url.includes('aman'));
+
+
+console.log(gameName.split('-'));
